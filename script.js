a99c1fac1a6c6125f2cf601d06a5d356caad1bd7
Elisha <lostmatterlmao@gmail.com>
2024-03-18 22:56:47 -0500

    Initial Commit | Deployment

---
+++ script.js
@@ -0,0 +1,288 @@
+const btn = document.querySelector('.btn');
+const rarityMessage = document.getElementById('rarity-message');
+const chanceMessage = document.getElementById('chance-message');
+const clickCountElement = document.getElementById('click-count');
+const recentRngList = document.querySelector('.recent-rng-list');
+
+let clickCount = 0;
+let recentRngs = [];
+
+// Update the click count display
+clickCountElement.textContent = `Clicks: ${clickCount}`;
+
+// ----------------------------------------
+
+const rarityLevels = {
+  '1 in 3 chance': {
+    text: 'Common',
+    color: 'grey',
+  },
+  '1 in 4 chance': {
+    text: 'Uncommon',
+    color: 'green',
+  },
+  '1 in 8 chance': {
+    text: 'Natural',
+    color: 'limegreen',
+  },
+  '1 in 16 chance': {
+    text: 'Rare',
+    color: 'blue',
+  },
+  '1 in 32 chance': {
+    text: 'Divinus',
+    color: 'gold',
+  },
+  '1 in 64 chance': {
+    text: 'Crystallized',
+    color: 'purple',
+  },
+  '1 in 128 chance': {
+    text: 'Rage',
+    color: 'red',
+  },
+  '1 in 256 chance': {
+    text: 'Glacier',
+    color: 'cyan',
+  },
+  '1 in 300 chance': {
+    text: 'Wind',
+    color: 'lightblue',
+  },
+  '1 in 350 chance': {
+    text: 'Ruby',
+    color: 'darkred',
+  },
+  '1 in 512 chance': {
+    text: 'Gilded',
+    color: 'yellow',
+  },
+  '1 in 777 chance': {
+    text: 'Jackpot',
+    color: 'gold',
+  },
+  '1 in 1004 chance': {
+    text: 'Diaboli',
+    color: 'darkred',
+  },
+  '1 in 1024 chance': {
+    text: 'Precious',
+    color: 'lightblue',
+  },
+  '1 in 1111 chance': {
+    text: 'Undefined',
+    color: 'grey',
+  },
+  '1 in 2048 chance': {
+    text: 'Magnetic',
+    color: 'lightblue',
+  },
+  '1 in 4096 chance': {
+    text: 'Sidereum',
+    color: 'gold',
+  },
+  '1 in 4444 chance': {
+    text: 'Bleeding',
+    color: 'red',
+  },
+  '1 in 5000 chance': {
+    text: 'Lunar',
+    color: 'lightblue',
+  },
+  '1 in 5000 chance': {
+    text: 'Solar',
+    color: 'yellow',
+  },
+  '1 in 6900 chance': {
+    text: 'Flushed',
+    color: 'yellow',
+  },
+  '1 in 6969 chance': {
+    text: 'awhecksno',
+    color: 'red',
+  },
+  '1 in 10000 chance': {
+    text: 'Undead',
+    color: 'darkgreen',
+  },
+  '1 in 12000 chance': {
+    text: 'Comet',
+    color: 'lightblue',
+  },
+  '1 in 12800 chance': {
+    text: 'HeatedRage',
+    color: 'red',
+  },
+  '1 in 24500 chance': {
+    text: 'Permafrost',
+    color: 'cyan',
+  },
+  '1 in 30000 chance': {
+    text: 'Stormal',
+    color: 'lightblue',
+  },
+  '1 in 30000 chance': {
+    text: 'Eclipse',
+    color: 'darkblue',
+  },
+  '1 in 40000 chance': {
+    text: 'Aquatic',
+    color: 'lightblue',
+  },
+  '1 in 69000 chance': {
+    text: 'Lobotomy',
+    color: 'green',
+  },
+  '1 in 70000 chance': {
+    text: 'Nautilus',
+    color: 'lightblue',
+  },
+  '1 in 99999 chance': {
+    text: 'Exotic',
+    color: 'white',
+  },
+  '1 in 100000 chance': {
+    text: 'Devil Undead',
+    color: 'darkgreen',
+  },
+  '1 in 100400 chance': {
+    text: 'Voided Diaboli',
+    color: 'darkred',
+  },
+  '1 in 200000 chance': {
+    text: 'Bounded',
+    color: 'lightblue',
+  },
+  '1 in 350000 chance': {
+    text: 'Celestial',
+    color: 'lightblue',
+  },
+  '1 in 500000 chance': {
+    text: 'Galaxy',
+    color: 'lightblue',
+  },
+  '1 in 500000 chance': {
+    text: 'FULLMOON',
+    color: 'lightblue',
+  },
+  '1 in 600000 chance': {
+    text: 'Twilight',
+    color: 'lightblue',
+  },
+  '1 in 1000000 chance': {
+    text: 'Arcane',
+    color: 'lightblue',
+  },
+  '1 in 1100000 chance': {
+    text: 'STARCOURSE',
+    color: 'red',
+  },
+};
+
+// ----------------------------------------
+
+btn.addEventListener('click', () => {
+  if (isOnCooldown) return;
+  
+  clickCount++;
+  clickCountElement.textContent = `Clicks: ${clickCount}`;
+  setCooldown();
+
+  const chances = [
+    { text: '1 in 3 chance', weight: 0.33 },
+    { text: '1 in 4 chance', weight: 0.25 },
+    { text: '1 in 8 chance', weight: 0.125 },
+    { text: '1 in 16 chance', weight: 0.0625 },
+    { text: '1 in 32 chance', weight: 0.03125 },
+    { text: '1 in 64 chance', weight: 0.015625 },
+    { text: '1 in 128 chance', weight: 0.0078125 },
+    { text: '1 in 256 chance', weight: 0.00390625 },
+    { text: '1 in 300 chance', weight: 0.00333333333 },
+    { text: '1 in 350 chance', weight: 0.00285714285 },
+    { text: '1 in 512 chance', weight: 0.001953125 },
+    { text: '1 in 777 chance', weight: 0.00128534704 },
+    { text: '1 in 1004 chance', weight: 0.00099501661 },
+    { text: '1 in 1024 chance', weight: 0.0009765625 },
+    { text: '1 in 1111 chance', weight: 0.00090009001 },
+    { text: '1 in 2048 chance', weight: 0.00048828125 },
+    { text: '1 in 4096 chance', weight: 0.00024414062 },
+    { text: '1 in 4444 chance', weight: 0.00022502250 },
+    { text: '1 in 5000 chance', weight: 0.00020004000 },
+    { text: '1 in 6900 chance', weight: 0.00014492753 },
+    { text: '1 in 6969 chance', weight: 0.00014367816 },
+    { text: '1 in 10000 chance', weight: 0.00010000000 },
+    { text: '1 in 12000 chance', weight: 0.00008333333 },
+    { text: '1 in 12800 chance', weight: 0.00007812500 },
+    { text: '1 in 24500 chance', weight: 0.00004081632 },
+    { text: '1 in 30000 chance', weight: 0.00003333333 },
+    { text: '1 in 40000 chance', weight: 0.00002500000 },
+    { text: '1 in 69000 chance', weight: 0.00001449275 },
+    { text: '1 in 70000 chance', weight: 0.00001428571 },
+    { text: '1 in 99999 chance', weight: 0.00001000010 },
+    { text: '1 in 100000 chance', weight: 0.00001000000 },
+    { text: '1 in 100400 chance', weight: 0.00000996016 },
+    { text: '1 in 200000 chance', weight: 0.00000500000 },
+    { text: '1 in 350000 chance', weight: 0.00000285714 },
+    { text: '1 in 500000 chance', weight: 0.00000200000 },
+    { text: '1 in 600000 chance', weight: 0.00000166667 },
+    { text: '1 in 1000000 chance', weight: 0.00000100000 },
+    { text: '1 in 1100000 chance', weight: 0.00000090909 },
+  ];
+
+  const randomNumber = Math.random();
+  let totalWeight = 0;
+  for (const chance of chances) {
+    totalWeight += chance.weight;
+    if (randomNumber < totalWeight) {
+      const message = chance.text;
+      const rarity = rarityLevels[message];
+      rarityMessage.textContent = rarity.text;
+      rarityMessage.style.color = rarity.color;
+      rarityMessage.style.textShadow = '0 0 20px ' + rarity.color + ', 0 0 40px ' + rarity.color + ', 0 0 84px ' + rarity.color;
+
+      chanceMessage.textContent = message;
+
+      // Add the rarity to the recent RNGs list
+      recentRngs.unshift({
+        rarity: rarity.text,
+        chance: message,
+      });
+
+      // Limit the recent RNGs list to the last 5 items
+      if (recentRngs.length > 19) {
+        recentRngs.pop();
+      }
+
+      // Update the recent RNGs list in the HTML // use toboto font sans serif 400px weight
+      recentRngList.innerHTML = '';
+      for (const rng of recentRngs) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${rng.rarity}: ${rng.chance}`;
+        listItem.style.color = rarity.color;
+        listItem.style.fontFamily = 'sans-serif';
+        listItem.style.fontWeight = '700px';
+        
+        recentRngList.appendChild(listItem);
+      }
+
+      break;
+    }
+  }
+});
+
+
+const cooldownTime = 200;
+let isOnCooldown = false;
+let cooldownTimer;
+
+function resetCooldown() {
+  isOnCooldown = false;
+  clearTimeout(cooldownTimer);
+  document.getElementById('cooldown-message').textContent = '';
+}
+
+function setCooldown() {
+  isOnCooldown = true;
+  //document.getElementById('cooldown-message').textContent = 'Cooldown!';
+  cooldownTimer = setTimeout(resetCooldown, cooldownTime);
+}
